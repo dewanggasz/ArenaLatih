3985aae03fdfad2c56e44e1767d595e16f2cf1cf
dewanggasz <suryadewangga07@gmail.com>
2025-06-24 00:34:14 +0700

    feat: add middlewer in bootsrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->verifyCsrfToken(except: [
+            'diskusi/*/delete', // URL yang dikecualikan dari CSRF
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
